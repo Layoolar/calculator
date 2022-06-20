92c3f94c95a24e54d85757e7193c26a2fdc0076b
Layoolar <olaiwolaayoola@yahoo.com>
2022-06-20 20:11:03 +0100

    Encapsulation of all functions

--- calculate.py
+++ calculate.py
@@ -1,31 +1,7 @@
 from calculator import ThisCalculator
 # Program make a simple calculator
 
-# This function adds two numbers
-
-
-def add(x, y):
-    return x + y
-
-# This function subtracts two numbers
-
-
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-
-
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-
-
-def divide(x, y):
-    return x / y
-
-
+# Program make a simple calculator
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -35,30 +11,26 @@ print("4.Divide")
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
+    calcFunction = ThisCalculator()
 
+    print(calcFunction.handleChoice(choice))
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
 
-        if choice == '1':
-            newAdd = ThisCalculator()
-            print(num1, "+", num2, "=", newAdd.add(num1, num2))
+    # if choice == '1':
+    #     print(num1, "+", num2, "=", calcFunction.add(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+    # elif choice == '2':
+    #     print(num1, "-", num2, "=", calcFunction.subtract(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+    # elif choice == '3':
+    #     print(num1, "*", num2, "=", calcFunction.multiply(num1, num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+    # elif choice == '4':
+    #     print(num1, "/", num2, "=", calcFunction.divide(num1, num2))
 
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
+    # check if user wants another calculation
+    # break the while loop if answer is no
 
-    else:
-        print("Invalid Input")
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if next_calculation == "no" or next_calculation == "n":
+        break
--- calculator.py
+++ calculator.py
@@ -1,6 +1,40 @@
 from msilib.schema import Class
+from secrets import choice
 
 
 class ThisCalculator():
+
+    # This function adds two numbers
     def add(self, x, y):
         return x+y
+
+    # This function subtracts two numbers
+    def subtract(self, x, y):
+        return x - y
+
+    # This function multiplies two numbers
+    def multiply(self, x, y):
+        return x * y
+
+    # This function divides two numbers
+    def divide(self, x, y):
+        x / y
+
+    def handleOption(self, choice, x, y):
+        if choice == 1:
+            # return x, "+", y, "=", self.add(x, y)
+            return f'{x} + {y} = {self.add(x, y)}'
+        elif choice == 2:
+            return f'{x} - {y} = {self.subtract(x, y)}'
+        elif choice == 3:
+            return f'{x} * {y} = {self.multiply(x, y)}'
+        elif choice == 4:
+            return f'{x} / {y} = {self.divide(x, y)}'
+        else:
+            return "invalid"
+
+    def handleChoice(self, choice):
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            return self.handleOption(int(choice), num1, num2)
