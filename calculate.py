ead31195283404beed02e80e8b5adfdd612f0685
Layoolar <olaiwolaayoola@yahoo.com>
2022-06-20 22:11:49 +0100

    Encapsulation of all functions

--- calculate.py
+++ calculate.py
@@ -1,36 +1,6 @@
 from calculator import ThisCalculator
 # Program make a simple calculator
+calcFunction = ThisCalculator()
 
-# Program make a simple calculator
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-    calcFunction = ThisCalculator()
-
-    print(calcFunction.handleChoice(choice))
-    # check if choice is one of the four options
-
-    # if choice == '1':
-    #     print(num1, "+", num2, "=", calcFunction.add(num1, num2))
-
-    # elif choice == '2':
-    #     print(num1, "-", num2, "=", calcFunction.subtract(num1, num2))
-
-    # elif choice == '3':
-    #     print(num1, "*", num2, "=", calcFunction.multiply(num1, num2))
-
-    # elif choice == '4':
-    #     print(num1, "/", num2, "=", calcFunction.divide(num1, num2))
-
-    # check if user wants another calculation
-    # break the while loop if answer is no
-
-    next_calculation = input("Let's do next calculation? (yes/no): ")
-    if next_calculation == "no" or next_calculation == "n":
-        break
+calcFunction.initial()
+calcFunction.nextCalc()
--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 from msilib.schema import Class
 from secrets import choice
 
+# Class that handles the calculator app
+
 
 class ThisCalculator():
 
@@ -18,11 +20,11 @@ class ThisCalculator():
 
     # This function divides two numbers
     def divide(self, x, y):
-        x / y
+        return x / y
 
+    # Function that handles the choice input to determine which operation to run
     def handleOption(self, choice, x, y):
         if choice == 1:
-            # return x, "+", y, "=", self.add(x, y)
             return f'{x} + {y} = {self.add(x, y)}'
         elif choice == 2:
             return f'{x} - {y} = {self.subtract(x, y)}'
@@ -30,11 +32,34 @@ class ThisCalculator():
             return f'{x} * {y} = {self.multiply(x, y)}'
         elif choice == 4:
             return f'{x} / {y} = {self.divide(x, y)}'
-        else:
-            return "invalid"
+
+    # A function that requests for the inputs and passes it if the choice input is valid
 
     def handleChoice(self, choice):
         if choice in ('1', '2', '3', '4'):
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
             return self.handleOption(int(choice), num1, num2)
+        else:
+            return "Invalid input"
+
+    # A function that starts the chains. Receives the choice input and returns the answer
+    def initial(self):
+        print("Select operation.")
+        print("1.Add")
+        print("2.Subtract")
+        print("3.Multiply")
+        print("4.Divide")
+        choice = input("Enter choice(1/2/3/4): ")
+        print(f' {self.handleChoice(choice)}')
+
+    # A function to handle if you want to perform another calculation
+    def nextCalc(self):
+        state = True
+        while state:
+            next_calculation = input(
+                "Let's do next calculation? (yes or y/no or n): ")
+            if next_calculation == "no" or next_calculation == "n":
+                state = False
+            if next_calculation == "yes" or next_calculation == "y":
+                self.initial()
